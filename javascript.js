835b3eec354ce571ee46afd09e9a73e1374be440
Fahim Kabir <fkabir101@gmail.com>
2019-01-08 20:02:34 -0500

    Button Working

--- javascript.js
+++ javascript.js
@@ -1,12 +1,31 @@
-var search = "rwby"
-var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" +
-        search + "&api_key=ra70xorxvoPiBArlJQBpj3SmfmXSw2sX&limit=10";
-$.ajax({
-  url: queryUrl,
-  limit: 10,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-  var imageUrl = response.data[0].images.fixed_height.url;
-  $("#images").html("<img src="+imageUrl+">");
+// Declare initial variables
+var queryArray = ["RWBY", "Rooster Teeth", "Achievement Hunter"];
+
+// Run function initially to create buttons
+createButton();
+
+// function to create and recreate buttons
+function createButton() {
+  var buttonContainer = $("#buttons");
+  buttonContainer.html("");
+  for (var i = 0; i < queryArray.length; i++) {
+    var button = $("<button>");
+    button.text(queryArray[i]);
+    buttonContainer.append(button);
+  }
+}
+
+$("button").on("click", function (event) {
+  var search = $(this).text();
+  var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" +
+    search + "&api_key=ra70xorxvoPiBArlJQBpj3SmfmXSw2sX&limit=10";
+  $.ajax({
+    url: queryUrl,
+    limit: 15,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    var imageUrl = response.data[0].images.fixed_height.url;
+    $("#images").html("<img src=" + imageUrl + ">");
+  });
 });
